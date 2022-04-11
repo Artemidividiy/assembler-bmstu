85973b0ba2b67ddcbcb8c6a1a064f0d60d763500
uselessxiv <artemiy.kasyanik@gmail.com>
2022-04-11 16:20:11 +0300

    don't work, added lib64.asm

--- labs/1/solution.asm
+++ labs/1/solution.asm
@@ -7,8 +7,9 @@ lenExit equ $-ExitMsg
    X resd 1
 InBuf   resb    10            
 lenIn   equ     $-InBuf 
-        section .text        
-        global  _start
+        
+section .text        
+   global  _start
 _start:
       mov EAX, [A]
       add EAX, 5
@@ -17,8 +18,9 @@ _start:
         ; write
         mov     rax, 1        
         mov     rdi, 1       
-        mov     rsi, ExitMsg  
+        mov     rsi, [X]
         mov     rdx, lenExit    
+         jne IntToStr64  
         syscall                  
         ; read
         mov     rax, 0        
@@ -31,40 +33,4 @@ _start:
         xor     rdi, rdi          
         syscall
 
-IntToStr64: 
-         push   rdi
-         push   rbx
-         push   rdx
-         push   rcx
-		 push   rsi
-		 mov    byte[rsi],0 
-         cmp    eax,0
-         jge    .l1
-         neg    eax
-         mov    byte[rsi],'-'
-.l1      mov    byte[rsi+6],10
-         mov    rdi,5
-         mov    bx,10
-.again:  cwd           
-         div    bx    
-         add    dl,30h 
-         mov    [rsi+rdi],dl 
-         dec    rdi      
-                       
-         cmp    ax, 0  
-         jne    .again
-         mov    rcx, 6
-         sub    rcx, rdi
-		 mov    rax,rcx
-		 inc    rax    
-         inc    rsi    
-		 push   rsi
-         lea    rsi,[rsi+rdi]
-		 pop    rdi
-         rep movsb
-         pop    rsi  
-         pop    rcx
-         pop    rdx
-         pop    rbx
-         pop    rdi
-         ret                  
+%include "../lib64.asm"
