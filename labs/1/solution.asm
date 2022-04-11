163aadaac5a179bd96d0fa670213a7cd07c6b4f6
uselessxiv <artemiy.kasyanik@gmail.com>
2022-04-11 17:02:32 +0300

    reinital

--- labs/1/solution.asm
+++ labs/1/solution.asm
@@ -1,36 +1,25 @@
-   section .data              
-ExitMsg db "Press Enter to Exit",10  
-A dw -30 
-B dw 21 
-lenExit equ $-ExitMsg
+   section .data        ; секция проинициализированных данных      
+ExitMsg db "Press Enter to Exit",10  ; создаем переменную, хранящую в себе сообщение
+lenExit equ $-ExitMsg    ; создаем переменную, хранящую длину сообщения выше
    section .bss              
-   X resd 1
-InBuf   resb    10            
+InBuf   resb    10              ; буфер для вводимой строки    
 lenIn   equ     $-InBuf 
-        
-section .text        
-   global  _start
+        section .text            ; сегмент кода       
+        global  _start
 _start:
-      mov EAX, [A]
-      add EAX, 5
-      sub EAX, [B]
-      mov [X], EAX
         ; write
-        mov     rax, 1        
-        mov     rdi, 1       
-        mov     rsi, [X]
-        mov     rdx, lenExit    
-         jne IntToStr64  
+        mov     rax, 1             ; системная функция записи     
+        mov     rdi, 1              ;  stdout=1
+        mov     rsi, ExitMsg  ; адрес выводимой строки
+        mov     rdx, lenExit   ; длина выводимой строки
         syscall                  
         ; read
-        mov     rax, 0        
-        mov     rdi, 0        
-        mov     rsi, InBuf       
-        mov     rdx, lenIn      
+        mov     rax, 0             ; системная функция чтения
+        mov     rdi, 0              ; stdin=0
+        mov     rsi, InBuf       ; адрес буфера ввода
+        mov     rdx, lenIn      ; размер буфера
         syscall                  
         ; exit
-        mov     rax, 60       
-        xor     rdi, rdi          
-        syscall
-
-%include "../lib64.asm"
+        mov     rax, 60          ; системная функция 60 - завершение процесса
+        xor     rdi, rdi            ; код возврата 0
+        syscall                     
