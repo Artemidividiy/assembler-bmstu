777254a1931e5ff1501140aacd99c9dada9141b5
uselessXIV <artemiy.kasyanik@gmail.com>
2022-09-08 12:47:06 +0300

    4 works not properly

--- labs/4/solution.asm
+++ labs/4/solution.asm
@@ -1,221 +1,220 @@
-section .data ;Секция инициализированных данных
-    EnterMsg db 0xa
-    RowElement db '  ['
-    Num db 31h
-    DoubleDot db '] - '
-    LenRowElement equ $-RowElement
-    InitialMatrix db 10, 'Initial Matrix:', 10
-    LenInitialMatrix equ $-InitialMatrix
-    ResultMatrix db 10, 'Result Matrix: ', 10
-    LenResultMatrix equ $-ResultMatrix
-    DelNum db "Input number for delete: "
-    lenDel equ $-DelNum
-    RowInput db 'Enter '
-    RowNum db 31h
-    RowMsg db ' row of your Matrix', 10
-    LenRowInput equ $-RowInput
-    TabMsg db '  '
-    rows dw 5
-    indx dw 0                   
+section .data
+welcome:        db      "Input the size of the array; Input q to quit"
+w_len:          equ     $ - welcome
+in_msg:         db      0xa, "Enter N: "
+im_len:         equ     $ - in_msg
+num_msg:        db      "Enter the numbers", 10
+nm_len:         equ     $ - num_msg
+out_msg:        db      "Sorted array: "
+om_len:         equ     $ - out_msg
 
-section .bss ;Секция неинициализированных данных
-    Matrix times 30 resw 1
-    NewMatrix times 24 resw 1
-    i resb 1
-    N resw 1
-    InBuf resb 10
-    LenBuf equ $-InBuf
-    OutBuf resb 4
-    LenOut equ $-OutBuf
+section .bss
+n_str:          resb    4
+num_str:        resb    4
+num_arr:        resd    64
 
-section .text ;Секция кода 
+section .text
+        global _start
+print:
+        PUSH    rbx
+        PUSH    rax
+        mov     rax,4
+        mov     rbx,1
+        int     0x80
+        POP     rax
+        POP     rbx
+        ret
 
-    global  _start
-_start: 
+scan:
+        PUSH    rbx
+        PUSH    rax
+        mov     rax,3
+        mov     rbx,0
+        int     0x80
+        POP     rax
+        POP     rbx
+        ret
 
-    ; Ввод матрицы
-    mov rcx, 6
-    mov rbx, 0
-    input_row:
-        push rcx
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, RowInput
-        mov rdx, LenRowInput
-        syscall
-        jmp ChngNum
+xTen:
+        PUSH    rbx
+        mov     rbx,rax
+        shl     rax,2
+        add     rax,rbx
+        shl     rax,1
+        POP     rbx
+        ret
 
-        continue:
-            mov rcx, 5
-            input_column:
-                push rcx
-                push rbx        
-                mov rax, 1
-                mov rdi, 1
-                mov rsi, RowElement
-                mov rdx, LenRowElement
-                syscall
-                mov rax, 0
-                mov rdi, 0
-                mov rsi, InBuf
-                mov rdx, LenBuf
-                syscall
-                call StrToInt64
-                pop rbx
-                mov [ebx*2 + Matrix], ax
-                inc ebx
-                pop rcx
-                inc byte[Num]
-                loop input_column
-            pop rcx
-            loop input_row
-            jmp metka_N      
+byTen:
+        PUSH    rdx
+        PUSH    rcx
+        mov     rdx,0
+        mov     rcx,10
+        div     rcx
+        mov     rbx,rdx
+        POP     rcx
+        POP     rdx
+        ret
 
-    ChngNum:
-        inc byte[RowNum]
-        mov byte[Num], 31h
-        jmp continue
+toInt:
+        PUSH    rbx
+        mov     rax,0
+        mov     rbx,0
+    .loopStr:
+        call    xTen
+        PUSH    rdx
+        mov     rdx,0
+        mov     dl,byte[rcx+rbx]
+        sub     dl,0x30
+        add     rax,rdx
+        POP     rdx
+        inc     rbx
+        cmp     byte[rcx+rbx],0xa
+        jle     .return
+        cmp     rbx,rdx
+        jge     .return
+        jmp     .loopStr
+    .return:
+        POP     rbx
+        ret
 
-    metka_N:
-            ; Вывод сообщения об вводе номера столбца для удаления
-            mov     rax, 1    
-            mov     rdi, 1          
-            mov     rsi, DelNum   
-            mov     rdx, lenDel
-            syscall             
-            ;ввод номера столбца для удаления
-            mov     rax, 0         
-            mov     rdi, 0         
-            mov     esi, InBuf      
-            mov     rdx, LenBuf     
-            syscall
-            call StrToInt64
-            cmp EBX, 0
-            jne StrToInt64.Error
-            sub ax, 1
-            mov [N], ax ; Записали N
+toStr:
+        PUSH    rbx
+        PUSH    rax
+        mov     rbx,0
+        PUSH    0
+    .loopDiv:
+        call    byTen
+        add     rbx,0x30
+        PUSH    rbx
+        cmp     rax,0
+        jg      .loopDiv
+        mov     rbx,0
+    .loopStr:
+        POP     rax
+        cmp     rax,0
+        je      .loopFill
+        cmp     rbx,rdx
+        je      .loopStr
+        mov     byte[rcx+rbx],al
+        inc     rbx
+        jmp     .loopStr
+    .loopFill:
+        cmp     rbx,rdx
+        je      .return
+        mov     byte[rcx+rbx],0
+        inc     rbx
+        jmp     .loopFill
+    .return:
+        POP     rax
+        POP     rbx
+        ret
 
-    ; Вывод матрицы
-    print:
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, InitialMatrix
-        mov rdx, LenInitialMatrix
-        syscall
-        mov rbx, 0
-        mov rcx, 6
+sortN:  ;ebx=address of the array of numbers, ecx=array size(>0)
+        ;Array gets sorted descending order.
+        ;Equivalent C program (a = array; n = length):
+        ; i=n;
+        ; do {
+        ;     i--;
+        ;     j=i;
+        ;     do {
+        ;         j--;
+        ;         if (a[i]>a[j])
+        ;             swap(a[i],s[j]);
+        ;     } while(j>0);
+        ; } while (i>1);
+        PUSH    rax
+        PUSH    rsi ;esi and edi are used here like eax, ebx etc.; Nothing special!
+        PUSH    rdi
+        mov     rdi,rcx
+        shl     rdi,2
+    .loopN:
+        sub     rdi,4
+        mov     rax,qword[rbx+rdi]
+        mov     rsi,rdi
+    .loopIn:
+        sub     rsi,4
+        cmp     rax,qword[rbx+rsi]
+        jg      .swap
+        jmp     .continue
+    .swap:
+        PUSH    qword[rbx+rsi]
+        PUSH    qword[rbx+rdi]
+        POP     qword[rbx+rsi]
+        POP     qword[rbx+rdi]
+        mov     rax,qword[rbx+rdi]
+    .continue:
+        cmp     rsi,0
+        jg      .loopIn
+        cmp     rdi,4
+        jg      .loopN
 
-        row_cycle:
-            push rcx
-            mov rax, 1
-            mov rdi, 1
-            mov rsi, TabMsg
-            mov rdx, 2
-            syscall
-            mov rcx, 5
+        POP     rdi
+        POP     rsi
+        POP     rax
+        ret
 
-            column_cycle:
-                push rcx
-                push rbx
-                mov ax, [ebx*2 + Matrix]                
-                cwde
-                mov rsi, OutBuf
-                call IntToStr64
-                mov rdi, 1
-                mov rdx, rax
-                mov rax, 1
-                syscall
-                pop rbx
-                inc ebx
-                pop rcx
-                loop column_cycle        
-            mov rax, 1
-            mov rdi, 1
-            mov rsi, EnterMsg
-            mov rdx, 1
-            syscall
-            pop rcx 
-            loop row_cycle   
+_start:
+        mov     rcx,welcome
+        mov     rdx,w_len
+        call    print
+    .main_loop:
+        mov     rcx,in_msg ;Ask the user for N
+        mov     rdx,im_len
+        call    print
 
-        ; Блок вычислений
-        edit:  
-            mov rbx, 0
-            mov rsi, 0
-            mov rdx, 0
-            mov rdi, 0 
-            mov rcx, 5
+        mov     rcx,n_str ;Input N
+        mov     rdx,4
+        call    scan
 
-            edit_cycle1:
-                push rcx              
-                mov ax, [N]
-                cmp bx, ax
-                je skip
-                mov rcx, 6
+        cmp     byte[n_str],0x71 ;If 'q', quit
+        je      .quit
 
-                edit_cycle2:                
-                    push rcx
-                    mov ax, [esi + ebx*2 + Matrix]
-                    mov [edx + edi*2 + NewMatrix], ax
-                    add rsi, 10
-                    add rdx, 8
-                    pop rcx
-                    loop edit_cycle2
-                mov rsi, 0
-                mov rdx, 0
-                add rdi, 1
-                add rbx, 1
-                pop rcx 
-                loop edit_cycle1
-                jmp print_result  
-                skip: 
-                    mov rsi, 0
-                    mov rdx, 0
-                    add rbx, 1
-                    pop rcx 
-                    loop edit_cycle1
+        call    toInt ;get the integer N, PUSH it, and move it to ebx
+        PUSH    rax
+        mov     rbx,rax
 
-    ;вывод результирующей матрицы
-    print_result:        
-            mov rax, 1
-            mov rdi, 1
-            mov rsi, ResultMatrix
-            mov rdx, LenResultMatrix
-            syscall   
-            mov rbx, 0
-            mov rcx, 6
-            res_row_cycle:
-                push rcx
-                mov rax, 1
-                mov rdi, 1
-                mov rsi, TabMsg
-                mov rdx, 2
-                syscall
-                mov rcx, 4
-                res_column_cycle:                
-                    push rcx
-                    push rbx
-                    mov ax, [ebx*2 + NewMatrix]
-                    cwde
-                    mov rsi, OutBuf
-                    call IntToStr64
-                    mov rdi, 1
-                    mov rdx, rax
-                    mov rax, 1
-                    syscall
-                    pop rbx
-                    inc ebx
-                    pop rcx
-                    loop res_column_cycle
-                mov rax, 1
-                mov rdi, 1
-                mov rsi, EnterMsg
-                mov rdx, 1
-                syscall
-                pop rcx 
-                loop res_row_cycle
+        mov     rcx,num_msg ;Ask the user to input all numbers
+        mov     rdx,nm_len
+        call    print
 
-        ; Завершение работы программы
-            mov     rax, 60        
-            xor     rdi, rdi          
-            syscall
-%include "../lib64.asm"
+    .loopN_1: ;Input the numbers
+        dec     rbx
+        ;get the number and store it in [num_arr+ebx*4]
+        mov     rcx,num_str
+        mov     rdx,4
+        call    scan
+        call    toInt
+        mov     rdx,rbx
+        shl     rdx,2 ;edx = edx*4
+        mov     qword[num_arr+rdx], rax
+        cmp     rbx,0
+        jne     .loopN_1 ;Note: The numbers will be stored in reverse order of input
+
+        mov     rbx,num_arr
+        POP     rcx ;POP N to ecx
+        call    sortN
+        mov     rbx,rcx
+
+        mov     rcx,out_msg
+        mov     rdx,om_len
+        call    print
+
+    .loopN_2: ;Output the numbers
+        dec     rbx
+        mov     rdx,rbx
+        shl     rdx,2
+        mov     rax, qword[num_arr+rdx]
+        mov     rcx,num_str
+        mov     rdx,4
+        call    toStr
+        mov     byte[num_str+3],0x20 ;ascii code for space
+        call    print
+        cmp     rbx,0
+        jne     .loopN_2 ;Note: The numbers will be printed in reverse order too
+
+        jmp     .main_loop
+
+    .quit:
+        mov     rbx,0
+        mov     rax,1
+        int     0x80
